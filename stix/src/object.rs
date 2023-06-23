f6eebd2a9be0a3c9dd76c98ce6aa20d8d67bf050
tmaraval <tmaraval@randorisec.fr>
2023-06-23 14:24:20 +0200

    Derive Clone trait for Malware and Indicator struct

--- stix/src/object.rs
+++ stix/src/object.rs
@@ -5,124 +5,124 @@ use serde::Deserialize;
 
 use crate::{reference::ExternalReference, Confidence, Id};
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Clone)]
 pub struct CommonProperties {
-    id: Id,
-    created_by_ref: Option<Id>,
-    #[serde(default)]
-    revoked: bool,
-    #[serde(default)]
-    labels: BTreeSet<String>,
-    #[serde(default)]
-    object_marking_refs: BTreeSet<Id>,
-    #[serde(default)]
-    external_references: Vec<ExternalReference>,
-    #[serde(default)]
-    created: Option<DateTime<Utc>>,
-    #[serde(default)]
-    modified: Option<DateTime<Utc>>,
-    #[serde(default)]
-    confidence: Option<Confidence>,
+  id: Id,
+  created_by_ref: Option<Id>,
+  #[serde(default)]
+  revoked: bool,
+  #[serde(default)]
+  labels: BTreeSet<String>,
+  #[serde(default)]
+  object_marking_refs: BTreeSet<Id>,
+  #[serde(default)]
+  external_references: Vec<ExternalReference>,
+  #[serde(default)]
+  created: Option<DateTime<Utc>>,
+  #[serde(default)]
+  modified: Option<DateTime<Utc>>,
+  #[serde(default)]
+  confidence: Option<Confidence>,
 }
 
 /// Common properties for a STIX Domain Object.
 pub trait Object {
-    fn id(&self) -> &Id;
-    fn created_by_ref(&self) -> Option<&Id>;
-    fn revoked(&self) -> bool;
-    fn labels(&self) -> &BTreeSet<String>;
-    fn object_marking_refs(&self) -> &BTreeSet<Id>;
-    fn external_references(&self) -> &[ExternalReference];
-    fn created(&self) -> Option<&DateTime<Utc>>;
-    fn modified(&self) -> Option<&DateTime<Utc>>;
-    fn confidence(&self) -> Option<Confidence>;
+  fn id(&self) -> &Id;
+  fn created_by_ref(&self) -> Option<&Id>;
+  fn revoked(&self) -> bool;
+  fn labels(&self) -> &BTreeSet<String>;
+  fn object_marking_refs(&self) -> &BTreeSet<Id>;
+  fn external_references(&self) -> &[ExternalReference];
+  fn created(&self) -> Option<&DateTime<Utc>>;
+  fn modified(&self) -> Option<&DateTime<Utc>>;
+  fn confidence(&self) -> Option<Confidence>;
 }
 
 impl Object for CommonProperties {
-    fn id(&self) -> &Id {
-        &self.id
-    }
+  fn id(&self) -> &Id {
+    &self.id
+  }
 
-    fn created_by_ref(&self) -> Option<&Id> {
-        self.created_by_ref.as_ref()
-    }
+  fn created_by_ref(&self) -> Option<&Id> {
+    self.created_by_ref.as_ref()
+  }
 
-    fn external_references(&self) -> &[ExternalReference] {
-        &self.external_references
-    }
+  fn external_references(&self) -> &[ExternalReference] {
+    &self.external_references
+  }
 
-    fn created(&self) -> Option<&DateTime<Utc>> {
-        self.created.as_ref()
-    }
+  fn created(&self) -> Option<&DateTime<Utc>> {
+    self.created.as_ref()
+  }
 
-    fn modified(&self) -> Option<&DateTime<Utc>> {
-        self.modified.as_ref()
-    }
+  fn modified(&self) -> Option<&DateTime<Utc>> {
+    self.modified.as_ref()
+  }
 
-    fn revoked(&self) -> bool {
-        self.revoked
-    }
+  fn revoked(&self) -> bool {
+    self.revoked
+  }
 
-    fn labels(&self) -> &BTreeSet<String> {
-        &self.labels
-    }
+  fn labels(&self) -> &BTreeSet<String> {
+    &self.labels
+  }
 
-    fn object_marking_refs(&self) -> &BTreeSet<Id> {
-        &self.object_marking_refs
-    }
+  fn object_marking_refs(&self) -> &BTreeSet<Id> {
+    &self.object_marking_refs
+  }
 
-    fn confidence(&self) -> Option<Confidence> {
-        self.confidence
-    }
+  fn confidence(&self) -> Option<Confidence> {
+    self.confidence
+  }
 }
 
 impl<T: AsRef<CommonProperties>> Object for T {
-    fn id(&self) -> &Id {
-        self.as_ref().id()
-    }
+  fn id(&self) -> &Id {
+    self.as_ref().id()
+  }
 
-    fn created_by_ref(&self) -> Option<&Id> {
-        self.as_ref().created_by_ref()
-    }
+  fn created_by_ref(&self) -> Option<&Id> {
+    self.as_ref().created_by_ref()
+  }
 
-    fn revoked(&self) -> bool {
-        self.as_ref().revoked()
-    }
+  fn revoked(&self) -> bool {
+    self.as_ref().revoked()
+  }
 
-    fn labels(&self) -> &BTreeSet<String> {
-        self.as_ref().labels()
-    }
+  fn labels(&self) -> &BTreeSet<String> {
+    self.as_ref().labels()
+  }
 
-    fn object_marking_refs(&self) -> &BTreeSet<Id> {
-        self.as_ref().object_marking_refs()
-    }
+  fn object_marking_refs(&self) -> &BTreeSet<Id> {
+    self.as_ref().object_marking_refs()
+  }
 
-    fn external_references(&self) -> &[ExternalReference] {
-        self.as_ref().external_references()
-    }
+  fn external_references(&self) -> &[ExternalReference] {
+    self.as_ref().external_references()
+  }
 
-    fn created(&self) -> Option<&DateTime<Utc>> {
-        self.as_ref().created()
-    }
+  fn created(&self) -> Option<&DateTime<Utc>> {
+    self.as_ref().created()
+  }
 
-    fn modified(&self) -> Option<&DateTime<Utc>> {
-        self.as_ref().modified()
-    }
+  fn modified(&self) -> Option<&DateTime<Utc>> {
+    self.as_ref().modified()
+  }
 
-    fn confidence(&self) -> Option<Confidence> {
-        self.as_ref().confidence()
-    }
+  fn confidence(&self) -> Option<Confidence> {
+    self.as_ref().confidence()
+  }
 }
 
 /// A STIX object associated with a specific ID type.
 ///
 /// All instances of the struct should have this as their ID.
 pub trait TypedObject {
-    /// The kebab-case type used as the object's ID prefix and in the `type` field
-    /// for declarations of the object.
-    ///
-    /// # Examples
-    /// * `course-of-action`
-    /// * `intrusion-set`
-    const TYPE: &'static str;
+  /// The kebab-case type used as the object's ID prefix and in the `type` field
+  /// for declarations of the object.
+  ///
+  /// # Examples
+  /// * `course-of-action`
+  /// * `intrusion-set`
+  const TYPE: &'static str;
 }
